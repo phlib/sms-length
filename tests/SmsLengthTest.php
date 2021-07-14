1388dc8f9dbe092e7c4937c531d01430aca278f0
atymic <atymicq@gmail.com>
2021-07-14 13:21:33 +1000

    fix: straddle message bug

--- tests/SmsLengthTest.php
+++ tests/SmsLengthTest.php
@@ -88,6 +88,21 @@ class SmsLengthTest extends TestCase
 
             // empty
             'empty' => ['', '7-bit', 0, 1, 160],
+
+            [
+                'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown f[x jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the.',
+                '7-bit',
+                306,
+                3,
+                459,
+            ],
+            [
+                str_repeat('🌐', 67),
+                'ucs-2',
+                134,
+                3,
+                201
+            ],
         ];
     }
 
