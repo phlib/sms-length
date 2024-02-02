a83f640fb920952f6cd86fea3ab9c0066de51c52
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2024-02-02 17:08:14 +0000

    Upgrade ECS to v12.1
    New config syntax.
    'common' no longer includes 'strict'.

--- ecs.php
+++ ecs.php
@@ -2,25 +2,34 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+return ECSConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
+    ])
 
-    $services = $containerConfigurator->services();
+    ->withSets([
+        SetList::COMMON,
+        SetList::PSR_12,
+        SetList::STRICT,
+    ])
 
-    $containerConfigurator->import(SetList::COMMON);
-    // Remove sniff, from common/control-structures
-    $services->remove(\PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer::class);
-    // Remove sniff, from common/spaces
-    $services->remove(\PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class);
-    $services->remove(\PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class);
+    ->withSkip([
+        // Remove sniff, from common/control-structures
+        \PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer::class,
 
-    $containerConfigurator->import(SetList::PSR_12);
-};
+        // Remove sniff, from common/spaces
+        \PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class,
+        \PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
+    ])
+
+    // PER Coding Style 7.1: "The `fn` keyword MUST NOT be succeeded by a space."
+    ->withConfiguredRule(
+        \PhpCsFixer\Fixer\FunctionNotation\FunctionDeclarationFixer::class,
+        [
+            'closure_fn_spacing' => 'none',
+        ]
+    );
